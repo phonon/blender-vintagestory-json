72e461d3fe7fbeced9c97e811031c78eb93e344b
phonon <phonon9000@gmail.com>
2023-04-30 16:43:19 -0400

    add uv single pixel (solid square) unwrap operator

--- vintagestory_utils/uv.py
+++ vintagestory_utils/uv.py
@@ -15,6 +15,7 @@ class OpUVCuboidUnwrap(bpy.types.Operator):
     def draw(self, context):
         pass
 
+
 class OpUVPixelUnwrap(bpy.types.Operator):
     """Unwrap all UVs into a single pixel (for single color textures)"""
     bl_idname = "vintagestory.uv_pixel_unwrap"
@@ -22,8 +23,37 @@ class OpUVPixelUnwrap(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context):
-        args = self.as_keywords()
-        print(args)
+        # args = self.as_keywords()
+
+        # need to be in object mode to access context selected objects
+        user_mode = context.active_object.mode
+        if user_mode != "OBJECT":
+            need_to_switch_mode_back = True
+            bpy.ops.object.mode_set(mode="OBJECT")
+        else:
+            need_to_switch_mode_back = False
+        
+        # only perform on selected objects
+        objects = bpy.context.selected_objects
+        for obj in objects:
+            mesh = obj.data
+            if not isinstance(mesh, bpy.types.Mesh):
+                continue
+            
+            uv_layer = mesh.uv_layers.active.data
+
+            for face in mesh.polygons:
+                loop_start = face.loop_start
+                # TODO: if object has texture, scale UVs to fit 1 pixel in texture.
+                # otherwise, set to unit square (0,0) to (1,1)
+                uv_layer[loop_start].uv = (0.0, 0.0)
+                uv_layer[loop_start+1].uv = (1.0, 0.0)
+                uv_layer[loop_start+2].uv = (1.0, 1.0)
+                uv_layer[loop_start+3].uv = (0.0, 1.0)
+        
+        if need_to_switch_mode_back:
+            bpy.ops.object.mode_set(mode=user_mode)
+        
         return {"FINISHED"}
     
     def draw(self, context):
